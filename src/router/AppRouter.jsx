639b92999b1b764dec29336ae307fe3edc1b56e4
Matías <matiasagbenitez@gmail.com>
2023-04-17 09:06:15 -0300

    configuración de rutas principales y secundarias

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { JournalRoutes } from "../journal/routes/JournalRoutes";
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      {/* LOGIN Y REGISTRO */}
+      <Route path="/auth/*" element={<AuthRoutes />} />
+
+      {/* JOURNAL APP */}
+      <Route path="/*" element={<JournalRoutes />} />
+    </Routes>
+  );
+};
