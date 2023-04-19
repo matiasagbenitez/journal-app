b576476a0e79eed93c491397a5e37c59320b2705
Matías <matiasagbenitez@gmail.com>
2023-04-19 20:16:02 -0300

    custom hook para mantener sesión iniciada

---
+++ src/hooks/useCheckAuth.js
@@ -0,0 +1,20 @@
+
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, login } from "../store/auth";
+import { FirebaseAuth } from "../firebase/config";
+
+export const useCheckAuth = () => {
+    const { status } = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        onAuthStateChanged(FirebaseAuth, async (user) => {
+            if (!user) return dispatch(logout);
+            const { displayName, email, uid, photoURL } = user;
+            dispatch(login({ displayName, email, uid, photoURL }));
+        });
+    }, []);
+
+    return { status };
+}
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,23 +1,11 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useCheckAuth } from "../hooks";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
 import { CheckingAuth } from "../ui";
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { FirebaseAuth } from "../firebase/config";
-import { login, logout } from "../store/auth";
 
 export const AppRouter = () => {
-  const { status } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    onAuthStateChanged(FirebaseAuth, async (user) => {
-      if (!user) return dispatch(logout);
-      const { displayName, email, uid, photoURL } = user;
-      dispatch(login({ displayName, email, uid, photoURL }));
-    });
-  }, []);
+  const { status } = useCheckAuth();
 
   if (status === "checking") {
     return <CheckingAuth />;
@@ -26,10 +14,10 @@ export const AppRouter = () => {
     <Routes>
       {status === "authenticated" ? (
         <Route path="/*" element={<JournalRoutes />} />
-        ) : (
+      ) : (
         <Route path="/auth/*" element={<AuthRoutes />} />
       )}
-      <Route path="/*" element={<Navigate to='/auth/login' />} />
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
 
       {/* <Route path="/auth/*" element={<AuthRoutes />} />        */}
       {/* <Route path="/*" element={<JournalRoutes />} />          */}
