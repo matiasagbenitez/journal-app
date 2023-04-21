c50259d1d9b759e88db6d3497f18de4e07156304
Matías <matiasagbenitez@gmail.com>
2023-04-21 19:51:10 -0300

    test carga de imagen y eliminación con SDK de cloudinary

--- src/helpers/fileUpload.js
+++ src/helpers/fileUpload.js
@@ -1,4 +1,5 @@
 export const fileUpload = async (file) => {
+    if (!file) return null;
     const cloudUrl = 'https://api.cloudinary.com/v1_1/drvyc6yyn/upload';
     const formData = new FormData();
     formData.append('upload_preset', 'journal-APP');
@@ -14,6 +15,7 @@ export const fileUpload = async (file) => {
         return cloudResp.secure_url;
     }
     catch (error) {
-        throw new Error(error.message);
+        return null;
+        // throw new Error(error.message);
     }
 }
---
+++ src/tests/helpers/fileUpload.test.js
@@ -0,0 +1,35 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { fileUpload } from "../../helpers/fileUpload";
+
+cloudinary.config({
+    cloud_name: 'drvyc6yyn',
+    api_key: '148976646438218',
+    api_secret: 'isU3PLNVx3t9ivpH5SszSmpSuso',
+    secure: true
+});
+
+describe('Pruebas en fileUpload', () => {
+    test('debe de subir un archivo correctamente a cloudinary', async () => {
+        const imageUrl = 'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png';
+        const resp = await fetch(imageUrl);
+        const blob = await resp.blob();
+        const file = new File([blob], 'foto.jpg');
+        const url = await fileUpload(file);
+        expect(typeof url).toBe('string');
+
+        // Borrar imagen por ID
+        const segments = url.split('/');
+        const imageId = segments[segments.length - 1].replace('.png', '');
+        const cloudResp = await cloudinary.api.delete_resources(['journal/' + imageId], {
+            resource_type: 'image'
+        });
+        
+    })
+
+    test('debe de retornar null', async () => {
+        const file = new File([], 'foto.jpg');
+        const url = await fileUpload(file);
+        expect(url).toBe(null);
+
+    })
+})
